2cf2ff7ac24625da672a7e7999b837eb3f1a982b
xshady <fastik036@gmail.com>
2022-06-20 03:25:26 +0300

    disable logs for published version

--- shared/src/logger.ts
+++ shared/src/logger.ts
@@ -1,7 +1,9 @@
-import { Logger as AltvLogger } from "altv-xlogger"
+import { Logger as AltvLogger, LogLevel } from "altv-xlogger"
 
 export class Logger extends AltvLogger {
   constructor(name: string) {
-    super(`xpeds-sync > ${name}`)
+    super(`xpeds-sync > ${name}`, {
+      logLevel: ___DEV_MODE ? LogLevel.Info : LogLevel.Warn,
+    })
   }
 }
